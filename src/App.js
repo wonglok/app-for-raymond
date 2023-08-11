69b731c5c72b6f56592102f15130a66f0167cc60
Wong Lok <wonglok@users.noreply.github.com>
2023-08-11 22:42:37 +0800

    app

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import "./App.css";
 
 import { Environment, Html, Text, useGLTF } from "@react-three/drei";
 import { Canvas, createPortal, useFrame, useThree } from "@react-three/fiber";
-import { Suspense, useEffect, useMemo } from "react";
+import { Suspense, useEffect, useMemo, useState } from "react";
 import {
   AnimationMixer,
   Box2,
@@ -17,6 +17,7 @@ import {
 } from "three";
 
 function App() {
+  let [api, setAPI] = useState(false);
   return (
     <div style={{ width: `256px`, height: `256px` }}>
       <Canvas
@@ -25,10 +26,34 @@ function App() {
         }}
       >
         <Suspense fallback={null}>
-          <Content></Content>
+          <AgapeEngine
+            onReady={(api) => {
+              setAPI(api);
+            }}
+          ></AgapeEngine>
         </Suspense>
       </Canvas>
 
+      {api && (
+        <button
+          onClick={async () => {
+            for (let i = 0; i < 250; i++) {
+              let result = api.onEachFrame({
+                currentTime: api.duration * (i / 250),
+                frameNumber: i,
+                totalFrame: 250,
+              });
+
+              await new Promise((resolve) => {
+                requestAnimationFrame(resolve);
+              });
+              console.log(result);
+            }
+          }}
+        >
+          Render All
+        </button>
+      )}
       <div
         style={{ position: "fixed", top: `0px`, left: `0px` }}
         id="boundingBOX"
@@ -65,7 +90,7 @@ function toScreenPosition(camera, renderer, offset = new Vector3()) {
   return new Vector2(vector.x, vector.y);
 }
 
-function Content({ onReady = () => {} }) {
+function AgapeEngine({ onReady = () => {} }) {
   let gltf = useGLTF(`/compress.glb`);
 
   let mixer = useMemo(() => {
@@ -103,58 +128,78 @@ function Content({ onReady = () => {} }) {
       activeCam.getWorldPosition(st.camera.position);
       activeCam.getWorldQuaternion(st.camera.quaternion);
     }
-
-    gltf.scene.traverse((it) => {
-      if (it.name === busName && !movingObject) {
-        movingObject = it;
-      }
-    });
-
-    if (movingObject) {
-      boxHelper.setFromObject(movingObject);
-
-      box3.makeEmpty();
-      box2.makeEmpty();
-      movingObject.updateMatrixWorld(true);
-      for (let i = 0; i < boxHelper.geometry.attributes.position.count; i++) {
-        pt.fromBufferAttribute(boxHelper.geometry.attributes.position, i);
-        // pt.applyMatrix4(movingObject.matrixWorld);
-        let expand = toScreenPosition(st.camera, st.gl, pt);
-        box2.expandByPoint(expand);
-      }
-
-      box3.getBoundingSphere(sph);
-
-      let boundingBOX = document.querySelector("#boundingBOX");
-      box2.getSize(size2d);
-      let sizerX = size2d.x;
-      let sizerY = size2d.y;
-
-      if (boundingBOX) {
-        boundingBOX.style.left = `${box2.min.x}px`;
-        boundingBOX.style.top = `${box2.min.y}px`;
-        boundingBOX.style.width = `${sizerX}px`;
-        boundingBOX.style.height = `${sizerY}px`;
-        boundingBOX.style.border = "rgba(255,0,0,1) solid 1px";
-      }
-      let dataURL = st.gl.getContext().canvas.toDataURL();
-      console.table([
-        { dataURL, sizerX, sizerY, x: box2.min.x, y: box2.min.y },
-      ]);
-    }
   });
+
+  let get = useThree((r) => r.get);
 
   useEffect(() => {
     let api = {
-      onEachFrame: () => {
-        let dt = 1 / 60;
-        mixer.update(dt);
+      duration: animations[0].duration,
+      onEachFrame: ({ currentTime = 0, frameNumber = 0, totalFrame = 1 }) => {
+        mixer.setTime(currentTime);
+
+        let st = get();
+
+        gltf.scene.traverse((it) => {
+          if (it.name === busName && !movingObject) {
+            movingObject = it;
+          }
+        });
+
+        if (movingObject) {
+          boxHelper.setFromObject(movingObject);
+
+          box3.makeEmpty();
+          box2.makeEmpty();
+          movingObject.updateMatrixWorld(true);
+          for (
+            let i = 0;
+            i < boxHelper.geometry.attributes.position.count;
+            i++
+          ) {
+            pt.fromBufferAttribute(boxHelper.geometry.attributes.position, i);
+            // pt.applyMatrix4(movingObject.matrixWorld);
+            let expand = toScreenPosition(st.camera, st.gl, pt);
+            box2.expandByPoint(expand);
+          }
+
+          box3.getBoundingSphere(sph);
+
+          let boundingBOX = document.querySelector("#boundingBOX");
+          box2.getSize(size2d);
+          let sizerX = size2d.x;
+          let sizerY = size2d.y;
+
+          if (boundingBOX) {
+            boundingBOX.style.left = `${box2.min.x}px`;
+            boundingBOX.style.top = `${box2.min.y}px`;
+            boundingBOX.style.width = `${sizerX}px`;
+            boundingBOX.style.height = `${sizerY}px`;
+            boundingBOX.style.border = "rgba(255,0,0,1) solid 1px";
+          }
+
+          st.gl.render(st.scene, st.camera);
+
+          let dataURL = st.gl.getContext().canvas.toDataURL();
+
+          return {
+            dataURL,
+            sizerX,
+            sizerY,
+            x: box2.min.x,
+            y: box2.min.y,
+            frameNumber,
+            totalFrame,
+          };
+        }
       },
     };
     onReady(api);
 
-    return () => {};
-  });
+    return () => {
+      //
+    };
+  }, [get]);
 
   return (
     <>
